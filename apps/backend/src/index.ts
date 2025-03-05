219594e3a00c3c53b93d2c7813b14cbf09b905a7
Leo <leo.m.troche@gmail.com>
2025-03-05 18:08:59 -0300

    backend: first commit

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,16 @@
+import cors from 'cors'
+import express, { json } from 'express'
+import { SERVER_PORT } from './config/env'
+
+const app = express()
+
+app.use(cors())
+app.use(json())
+
+app.get('/api', (_, res) => {
+	res.status(200).json({ message: 'Express + TS' })
+})
+
+app.listen(SERVER_PORT, () => {
+	console.log(`Listening on http://localhost:${SERVER_PORT}`)
+})
