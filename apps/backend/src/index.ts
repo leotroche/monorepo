5d543e3707b58bf28df7a21554eacbd0658375f9
Leo <leo.m.troche@gmail.com>
2025-03-12 14:01:32 -0300

    first commit

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,16 @@
+import cors from 'cors'
+import express, { json } from 'express'
+import { PORT } from '~/config/env'
+
+const app = express()
+
+app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(json())
+
+app.get('/api', (_, res) => {
+	res.status(200).json({ message: 'Express + TS' })
+})
+
+app.listen(PORT, () => {
+	console.log(`Listening on http://localhost:${PORT}`)
+})
---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+	return (
+		<div className="grid min-h-dvh place-items-center">
+			<h1 className="text-4xl">React + TS + Tailwind CSS</h1>
+		</div>
+	)
+}
