3262b4906c91ff8e3a9138c78ab2629965df567c
Leo <leo.m.troche@gmail.com>
2025-04-24 17:22:55 -0300

    chore(server): update env.ts

--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -1,4 +1,4 @@
-const NODE_ENV = process.env.NODE_ENV || 'development' // 'development' | 'production' | 'test'
+const NODE_ENV = process.env.NODE_ENV || 'development' // 'development' | 'production'
 const PORT = Number(process.env.PORT) || 3000
 
 export const env = {
