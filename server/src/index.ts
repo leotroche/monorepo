676ff9d42065324e526740fb193770c28bf3d174
Leonardo Troche <leo.m.troche@gmail.com>
2026-02-04 16:22:58 -0300

    Reorder middleware usage in index.ts to apply CORS before logger

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,8 @@ import { logger } from 'hono/logger'
 
 const app = new Hono()
 
-app.use('*', cors())
 app.use('*', logger())
+app.use('*', cors())
 
 app.get('/', (c) => {
   return c.json({ message: 'Hello, World!' })
