8b15a0935c749c946f441fab62d3351cd8ae4ac1
Leonardo Troche <leo.m.troche@gmail.com>
2026-02-04 14:47:17 -0300

    Update middleware usage in index.ts to apply CORS and logger to all routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
 
 const app = new Hono()
 
-app.use(cors())
+app.use('*', cors())
+app.use('*', logger())
 
 app.get('/', (c) => {
   return c.json({ message: 'Hello, World!' })
